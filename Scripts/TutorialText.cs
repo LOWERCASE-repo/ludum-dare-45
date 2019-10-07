1749ec1eb6e71800cc1e8a5aaab39b39abc09e74
LOWERCASE <52083329+LOWERCASE-repo@users.noreply.github.com>
2019-10-07 16:16:44 -0400

    starting an entire dialogue system one hour before submisison

--- Scripts/TutorialText.cs
+++ Scripts/TutorialText.cs
@@ -2,19 +2,4 @@ using UnityEngine;
 
 internal class TutorialText : MonoBehaviour {
   
-  [SerializeField]
-  private Color color;
-  
-  public void OnClick() {
-    crafter.AddInput(item);
-  }
-  
-  internal void Complete() {
-    button.enabled = false;
-    border.color = color;
-  }
-  
-  private void Start() {
-    SetItem(item);
-  }
 }
